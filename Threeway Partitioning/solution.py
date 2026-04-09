35189a18b7a1070231662101aa30e7681de70ff5
Aryan Rathi <rathiaryan48@gmail.com>
2026-04-10 01:12:42 +0530

    Create solution.py

---
+++ Threeway Partitioning/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    #Function to partition the array around the range such 
+    #that array is divided into three parts.
+	def threeWayPartition(self, arr, a, b):
+	    # code here 
+	    arr.sort()
+	    return arr
