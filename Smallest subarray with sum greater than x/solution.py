5e282698490eb6355bbd1fa7757d9f09f9d16d9b
Aryan Rathi <rathiaryan48@gmail.com>
2026-04-10 00:54:19 +0530

    Implement solution for smallest subarray with sum greater than x

---
+++ Smallest subarray with sum greater than x/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestSubWithSum(self, x, arr):
+        n = len(arr)
+        min_len = n + 1
+        current_sum = 0
+        left = 0
+        for right in range(n):
+            current_sum += arr[right]
+            while current_sum > x:
+                min_len = min(min_len, right - left + 1)
+                current_sum -= arr[left]
+                left += 1
+        return min_len if min_len <= n else 0
